22e45e9da257237d7bdf495b3aee160c470f857d
Akmal khan <154743921+Akmalkhan0@users.noreply.github.com>
2025-08-04 18:59:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,8 @@ function App() {
   const [error, setError] = useState('');
   const [mapCoords, setMapCoords] = useState(null);
   const [theme, setTheme] = useState('dark'); // 'light' or 'dark'
-
-  const openWeatherMapApiKey = process.env.VITE_OPEN_WEATHER;
-  const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS;
+  const openWeatherMapApiKey = 4de5be02c2439750e1d74f052a120068;
+  const googleMapsApiKey = AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao;
 
   useEffect(() => {
     document.body.className = theme;
